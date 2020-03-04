4efee2b5b9d4b22f5468dcbc8b69c2eeef33e257
Daniel Angulo <dangulo4@gmail.com>
2020-03-04 15:09:50 -0800

    app,js with new file structure

---
+++ Develop/app.js
@@ -0,0 +1,165 @@
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
+
+const render = require("./lib/htmlRenderer");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// Prompt user for questions
+function promptUser() {
+    return inquirer.prompt([{
+            // Prompt for manger name
+            type: 'input',
+            name: 'managerName',
+            message: 'Enter the manager name'
+        },
+        {
+            // Prompt for manger id
+            type: 'input',
+            name: 'managerId',
+            message: 'Enter manager Id'
+        },
+        {
+            // Prompt for manager email
+            type: 'input',
+            name: 'managerEmail',
+            message: 'Enter manager email'
+        },
+        {
+            // Prompt for office number
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Enter office number'
+        }
+    ]).then(function (answers) {
+        promptNewMember();
+    })
+}
+
+function promptNewMember() {
+    return inquirer.prompt([{
+        // Prompt for new team member
+        type: 'confirm',
+        name: 'getMore',
+        message: 'Would you like to enter a new team member?',
+        default: true,
+
+    }]).then(function (answers) {
+        console.log(answers);
+        //Only ask this info if the ans is YES 
+        if (answers.getMore) {
+            //Prompt for TEAM  INFO
+            promptNewMemberInfo();
+        } else {
+            //IF no exit the application 
+            process.exit();
+        }
+    })
+
+}
+
+function promptNewMemberInfo() {
+    return inquirer.prompt([
+    {
+        // Prompt for role
+        type: "checkbox",
+        message: "What is the user role?",
+        name: "role",
+        choices: [
+            "Engineer",
+            "Intern",
+        ],
+
+    }
+]).then(function (userChoice) {
+        // console.log(userChoice); 
+        if (userChoice.role == "Engineer") {
+            //CAll ADD ENGINEER CODE 
+            inquirer.prompt([{
+                    // Captures the Engineer name
+                    type: 'input',
+                    name: 'engineerName',
+                    message: 'Enter the engineer name'
+
+                },
+                {
+                    // Capture engineer id
+                    type: 'input',
+                    name: 'engineerId',
+                    message: 'Enter engineer Id'
+
+                },
+                {
+                    // Capture engineer email
+                    type: 'input',
+                    name: 'engineerEmail',
+                    message: 'Enter engineer email'
+
+                },
+                {
+                    // Capture github username
+                    type: 'input',
+                    name: 'github',
+                    message: 'Enter github username'
+
+                }
+            ]).then(function (engineerInfo) {
+                console.log("engineerInfo", engineerInfo);
+                promptNewMember();
+            })
+        } else {
+            if (userChoice.role == 'Intern') {
+
+                inquirer.prompt([{
+                        // Capture Intern info
+                        type: 'input',
+                        name: 'internName',
+                        message: 'Enter the intern name'
+
+                    },
+                    {
+                        // Prompt for intern id
+                        type: 'input',
+                        name: 'internId',
+                        message: 'Enter intern Id'
+
+                    },
+                    {
+                        // Prompt for intern email
+                        type: 'input',
+                        name: 'engineerEmail',
+                        message: 'Enter intern email'
+                    },
+                    {
+                        // Prompt for intern school
+                        type: 'input',
+                        name: 'github',
+                        message: 'Enter school'
+
+                    }
+                ]).then(function (internInfo) {
+                    console.log("internInfo", internInfo);
+                    promptNewMember();
+                })
+            }
+        }
+    })
+}
+
+async function init() {
+    try {
+        const data = await promptUser();
+        // const newData = await promptNewMember();
+    } catch (err) {
+        console.log(err);
+    }
+}
+init();
