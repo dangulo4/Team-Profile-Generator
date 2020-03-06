03ba1f5dcb495afe5e0500f7571207749d1532da
Daniel Angulo <dangulo4@gmail.com>
2020-03-06 00:01:31 -0800

    intern js file and created teamMember and arrayId arrays

--- Develop/app.js
+++ Develop/app.js
@@ -4,7 +4,7 @@ const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const path = require('path');
 const fs = require('fs');
-const util = require('util'); 
+const util = require('util');
 
 const OUTPUT_DIR = path.resolve(__dirname, 'output');
 const outputPath = path.join(OUTPUT_DIR, 'team.html');
@@ -13,164 +13,180 @@ const render = require("./lib/htmlRenderer");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-// Prompt user for questions
-function promptUser() {
-    return inquirer.prompt([{
-            // Prompt for manger name
-            type: 'input',
-            name: 'managerName',
-            message: 'Enter the manager name'
-        },
-        {
-            // Prompt for manger id
-            type: 'input',
-            name: 'managerId',
-            message: 'Enter manager Id'
-        },
-        {
-            // Prompt for manager email
-            type: 'input',
-            name: 'managerEmail',
-            message: 'Enter manager email'
-        },
-        {
-            // Prompt for office number
-            type: 'input',
-            name: 'officeNumber',
-            message: 'Enter office number'
-        }
-    ]).then(function (answers) {
-        console.log("answers", answers);
-        const manager = new Manager(
-            answers.managerName, 
-            answers.managerId, 
-            answers.managerEmail, 
-            answers.officeNumber);
-        promptNewMember();
-    })
-}
+const teamMembers = [];
+const idArray = [];
 
-function promptNewMember() {
-    return inquirer.prompt([{
-        // Prompt for new team member
-        type: 'confirm',
-        name: 'choice',
-        message: 'Would you like to enter a new team member?',
-        default: true,
+function getTeamMembers() {
+    // Prompt user for questions
+    function getManager() {
+        return inquirer.prompt([{
+                // Prompt for manger name
+                type: 'input',
+                name: 'managerName',
+                message: 'Enter the manager name'
+            },
+            {
+                // Prompt for manger id
+                type: 'input',
+                name: 'managerId',
+                message: 'Enter manager Id'
+            },
+            {
+                // Prompt for manager email
+                type: 'input',
+                name: 'managerEmail',
+                message: 'Enter manager email'
+            },
+            {
+                // Prompt for office number
+                type: 'input',
+                name: 'officeNumber',
+                message: 'Enter office number'
+            }
+        ]).then(function (answers) {
+            console.log("answers", answers);
+            const manager = new Manager(
+                this.managerName,
+                this.managerId,
+                this.managerEmail,
+                this.officeNumber);
+            teamMembers.push(answers.managerName);
+            idArray.push(answers.managerId);
+            console.log('Team Members: ' + teamMembers);
+            console.log('Team Member ID: ' + idArray);
+            //createTeam();
+            promptNewMember();
+        })
+    }
 
-    }]).then(function (answers) {
-        console.log(answers);
-        //Only ask this info if the ans is YES 
-        if (answers.choice) {
-            //Prompt for TEAM  INFO
-            promptNewMemberInfo();
-        } else {
-            //IF no exit the application 
-            console.log('Good Bye!');
-            process.exit();
-        }
-    })
+    function promptNewMember() {
+        return inquirer.prompt([{
+            // Prompt for new team member
+            type: 'confirm',
+            name: 'choice',
+            message: 'Would you like to enter a new team member?',
+            default: true,
 
-}
-
-function promptNewMemberInfo() {
-    return inquirer.prompt([
-    {
-        // Prompt for role
-        type: "checkbox",
-        message: "What is the user role?",
-        name: "role",
-        choices: [
-            "Engineer",
-            "Intern",
-        ],
+        }]).then(function (answers) {
+            console.log(answers);
+            //Only ask this info if the ans is YES 
+            if (answers.choice) {
+                //Prompt for TEAM  INFO
+                promptNewMemberInfo();
+            } else {
+                //IF no exit the application 
+                console.log('Good Bye!');
+                process.exit();
+            }
+        })
 
     }
-]).then(function (userChoice) {
-        // console.log(userChoice); 
-        if (userChoice.role == "Engineer") {
-            //CAll ADD ENGINEER CODE 
-            inquirer.prompt([{
-                    // Captures the Engineer name
-                    type: 'input',
-                    name: 'engineerName',
-                    message: 'Enter the engineer name'
 
-                },
-                {
-                    // Capture engineer id
-                    type: 'input',
-                    name: 'engineerId',
-                    message: 'Enter engineer Id'
+    function promptNewMemberInfo() {
+        return inquirer.prompt([{
+            // Prompt for role
+            type: "checkbox",
+            message: "What is the user role?",
+            name: "role",
+            choices: [
+                "Engineer",
+                "Intern",
+            ],
 
-                },
-                {
-                    // Capture engineer email
-                    type: 'input',
-                    name: 'engineerEmail',
-                    message: 'Enter engineer email'
-
-                },
-                {
-                    // Capture github username
-                    type: 'input',
-                    name: 'github',
-                    message: 'Enter github username'
-
-                }
-            ]).then(function (engineerInfo) {
-                console.log("engineerInfo", engineerInfo);
-                promptNewMember();
-            })
-        } else {
-            if (userChoice.role == 'Intern') {
-
+        }]).then(function (userChoice) {
+            // console.log(userChoice); 
+            if (userChoice.role == "Engineer") {
+                //CAll ADD ENGINEER CODE 
                 inquirer.prompt([{
-                        // Capture Intern info
+                        // Captures the Engineer name
                         type: 'input',
-                        name: 'internName',
-                        message: 'Enter the intern name'
+                        name: 'engineerName',
+                        message: 'Enter the engineer name'
+                    },
+                    {
+                        // Capture engineer id
+                        type: 'input',
+                        name: 'engineerId',
+                        message: 'Enter engineer Id'
 
                     },
                     {
-                        // Prompt for intern id
-                        type: 'input',
-                        name: 'internId',
-                        message: 'Enter intern Id'
-
-                    },
-                    {
-                        // Prompt for intern email
+                        // Capture engineer email
                         type: 'input',
                         name: 'engineerEmail',
-                        message: 'Enter intern email'
+                        message: 'Enter engineer email'
                     },
                     {
-                        // Prompt for intern school
+                        // Capture github username
                         type: 'input',
                         name: 'github',
-                        message: 'Enter school'
+                        message: 'Enter github username'
 
                     }
-                ]).then(function (internInfo) {
-                    console.log("internInfo", internInfo);
+                ]).then(function (engineerInfo) {
+                    // console.log("engineerInfo", engineerInfo);
+                    const engineer = new Engineer (
+                        this.engineerName,
+                        this.engineerId,
+                        this.engineerEmail,
+                        this.gitHub);
+                    teamMembers.push(engineerInfo.engineerName);
+                    idArray.push(engineerInfo.engineerId);
+                    console.log('Engineer info: ' + teamMembers);
+                    console.log('Engineer ID: ' + idArray);
                     promptNewMember();
                 })
+            } else {
+                if (userChoice.role == 'Intern') {
+
+                    inquirer.prompt([{
+                            // Capture Intern info
+                            type: 'input',
+                            name: 'internName',
+                            message: 'Enter the intern name'
+
+                        },
+                        {
+                            // Prompt for intern id
+                            type: 'input',
+                            name: 'internId',
+                            message: 'Enter intern Id'
+
+                        },
+                        {
+                            // Prompt for intern email
+                            type: 'input',
+                            name: 'internEmail',
+                            message: 'Enter intern email'
+                        },
+                        {
+                            // Prompt for intern school
+                            type: 'input',
+                            name: 'school',
+                            message: 'Enter school'
+
+                        }
+                    ]).then(function (internInfo) {
+                        console.log("internInfo", internInfo);
+                        const intern = new Intern (
+                            this.internName,
+                            this.internId,
+                            this.internEmail,
+                            this.school);
+                            teamMembers.push(internInfo.internName);
+                            idArray.push(internInfo.internId);
+                            console.log('Team Members: ' + teamMembers);
+                            console.log('Team Member ID: ' + idArray);
+                        promptNewMember();
+                    })
+                }
             }
-        }
-    })
-}
-
-async function init() {
-    try {
-        const data = await promptUser();
-        
-        
-
-    } catch (err) {
-        console.log(err);
+        })
     }
+    // Prompt for team manager
+    getManager();
 }
+// Call function to prompt for team information
+getTeamMembers();
 
-init();
-
+// write a function that does fswritefile 
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,6 +1,9 @@
 const Employee = require("./Employee")
 
 class Engineer extends Employee {
+  constructor(name, id, email) {
+    super(name, id, email)
+  }
   getRole() {
         return "Engineer";
       }
