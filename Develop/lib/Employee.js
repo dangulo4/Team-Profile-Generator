199264d26a44339a1e6a1a29653971b042dfd356
Daniel Angulo <dangulo4@gmail.com>
2020-03-05 20:01:39 -0800

    constructor and class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -6,23 +6,17 @@ class Employee {
       this.id = id;
       this.email = email;
     }
-  ​
     getName() {
       return this.name;
     }
-  ​
     getId() {
       return this.id;
     }
-  ​
     getEmail() {
       return this.email;
     }
-  ​
     getRole() {
       return "Employee";
     }
-  ​
   }
-  ​
   module.exports = Employee;
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,11 +1,16 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require('./Employee.js');
+
 class Manager extends Employee {
-    getRole() {
-        return "Manager";
-      }
-      getOffice() {
-        return this.officeNumber;
-      }
+  constructor(name, id, email)  {
+    super(name, id, email)
+  }
+  getRole() {
+    return "Manager";
+  }
+  getOffice() {
+    return this.officeNumber;
+  }
 }
 
 module.exports = Manager;
